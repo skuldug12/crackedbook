4b2c397bd4bcb4a4db178cf34519f66523404a87
skuldug12 <50291762+skuldug12@users.noreply.github.com>
2020-06-08 10:50:12 +0200

    Update crackedbook.sh

--- crackedbook.sh
+++ crackedbook.sh
@@ -86,13 +86,92 @@ ar x apt_1.8.2.1_amd64.deb
 tar -xvf data.tar.xz -C /
 rm -f *
 
-wget http://ftp.us.debian.org/debian/pool/main/g/gnutls28/libgnutls30_3.6.7-4+deb10u3_amd64.deb
-ar x libgnutls30_3.6.7-4+deb10u3_amd64.deb
+wget http://ftp.us.debian.org/debian/pool/main/a/adduser/adduser_3.118_all.deb
+ar x adduser_3.118_all.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/d/debconf/debconf_1.5.71_all.deb
+ar x debconf_1.5.71_all.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/p/perl/perl-base_5.28.1-6_amd64.deb
+ar x perl-base_5.28.1-6_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2019.1_all.deb
+ar x debian-archive-keyring_2019.1_all.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/g/gnupg2/gpgv_2.2.12-1+deb10u1_amd64.deb
+ar x gpgv_2.2.12-1+deb10u1_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/libg/libgcrypt20/libgcrypt20_1.8.4-5_amd64.deb
+ar x libgcrypt20_1.8.4-5_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/libg/libgpg-error/libgpg-error0_1.35-1_amd64.deb
+ar x libgpg-error0_1.35-1_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://security.debian.org/debian-security/pool/updates/main/a/apt/libapt-pkg5.0_1.8.2.1_amd64.deb
+ar x libapt-pkg5.0_1.8.2.1_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/l/lz4/liblz4-1_1.8.3-1_amd64.deb
+ar x liblz4-1_1.8.3-1_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/s/systemd/libsystemd0_241-7~deb10u4_amd64.deb
+ar x libsystemd0_241-7~deb10u4_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/s/systemd/libudev1_241-7~deb10u4_amd64.deb
+ar x libudev1_241-7~deb10u4_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/libz/libzstd/libzstd1_1.3.8+dfsg-3_amd64.deb
+ar x libzstd1_1.3.8+dfsg-3_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/g/glibc/libc6_2.28-10_amd64.deb
+ar x libc6_2.28-10_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://security.debian.org/debian-security/pool/updates/main/g/gnutls28/libgnutls30_3.6.7-4+deb10u4_amd64.deb
+ar x libgnutls30_3.6.7-4+deb10u4_amd64.deb
 tar -xvf data.tar.xz -C /
 rm -f *
 
 wget http://ftp.us.debian.org/debian/pool/main/g/gmp/libgmp10_6.1.2+dfsg-4_amd64.deb
 ar x libgmp10_6.1.2+dfsg-4_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+wget http://ftp.us.debian.org/debian/pool/main/n/nettle/libhogweed4_3.4.1-1_amd64.deb
+ar x libhogweed4_3.4.1-1_amd64.deb
+tar -xvf data.tar.xz -C /
+rm -f *
+
+tar -xvf data.tar.xz -C /
+rm -f *
+
+tar -xvf data.tar.xz -C /
+rm -f *
+
 tar -xvf data.tar.xz -C /
 rm -f *
 
